c4f3db0010321f3f76149c9b691007be714d9a45
Muhamad Ali Khan <ali7847khan@gmail.com>
2023-09-07 21:51:06 +0500

    Added my project

---
+++ index.js
@@ -0,0 +1,10 @@
+const  jsonfile = require('jsonfile');
+const moment = require('moment');
+
+const FILE_PATH = './data.json';
+const DATE  = moment().format();
+
+const data = {
+    date: DATE
+}
+jsonfile.writeFile(FILE_PATH, data); 
