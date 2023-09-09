d1ab12cfa4bc14c31a496b826d4b47045f733ea2
Muhamad Ali Khan <72930799+muhammadalizkhan@users.noreply.github.com>
2023-09-09 23:22:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const  jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
 
-
 const FILE_PATH = './data.json';
 const DATE  = moment().subtract(1, 'd').format();
 
